627e2f3d60d721fbe7d52bdd479d6b5786810067
Refrag <R3FR4GYT@gmail.com>
2024-07-29 17:28:05 +0200

    Add Twitch token refresh mechanism + move configs
    This commit adds auto refreshing of the Twitch OAuth Token. The token will be validated every 3 minutes and if it needs to be refreshed, it will be refreshed and saved back to the configuration file.
    This also moves the configuration files to a volume so that the changes made by the token refresher can persist.
    You will also now need to provide a refresh token in the Twitch configuration.

--- SAESRPredictionsBot/Program.cs
+++ SAESRPredictionsBot/Program.cs
@@ -4,21 +4,29 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using TwitchLib.Api;
+using TwitchLib.Api.Auth;
 using TwitchLib.Api.Core;
 using TwitchLib.Api.Core.Enums;
+using Timer = System.Timers.Timer;
 
 namespace SAESRPredictionsBot;
 
 internal class Program
 {
-    private static string ProgramDirectory = AppDomain.CurrentDomain.BaseDirectory;
+    private static string _twitchRefreshToken;
+
+    private static TwitchAPI _twitchApi;
+
+    private static Timer _refreshTwitchTokenTimer = new (180_000) { AutoReset = true };
+
+    private static TwitchSecrets _twitchSecrets;
     
     public static async Task Main(string[] args)
     {
-        string discordSecretsjson = File.ReadAllText(Path.Combine(ProgramDirectory, "discord-secrets"));
-        string twitchSecretsJson = File.ReadAllText(Path.Combine(ProgramDirectory, "twitch-secrets"));
+        string discordSecretsjson = File.ReadAllText("/config/discord-secrets");
         DiscordSecrets discordSecrets = JsonConvert.DeserializeObject<DiscordSecrets>(discordSecretsjson)!;
-        TwitchSecrets twitchSecrets = JsonConvert.DeserializeObject<TwitchSecrets>(twitchSecretsJson)!;
+        
+        _twitchSecrets = TwitchSecrets.Load();
         
         DiscordConfiguration discordConfiguration = new DiscordConfiguration()
         {
@@ -29,18 +37,24 @@ internal class Program
 
         ApiSettings twitchSettings = new ApiSettings()
         {
-            ClientId = twitchSecrets.ClientId,
-            AccessToken = twitchSecrets.AccessToken,
-            Secret = twitchSecrets.Secret,
+            ClientId = _twitchSecrets.ClientId,
+            Secret = _twitchSecrets.Secret,
+            AccessToken = _twitchSecrets.AccessToken,
             Scopes = new List<AuthScopes> { AuthScopes.Helix_Channel_Read_Predictions, AuthScopes.Helix_Channel_Manage_Predictions }
         };
+        _twitchRefreshToken = _twitchSecrets.RefreshToken;
         
         DiscordClient discordClient = new DiscordClient(discordConfiguration);
-        TwitchAPI twitchApi = new TwitchAPI(settings: twitchSettings);
+        _twitchApi = new TwitchAPI(settings: twitchSettings);
+
+        await RefreshTwitchToken();
+        
+        _refreshTwitchTokenTimer.Elapsed += (o, e) => RefreshTwitchToken();
+        _refreshTwitchTokenTimer.Start();
 
         ServiceCollection serviceCollection = new ServiceCollection();
-        serviceCollection.AddSingleton(twitchApi);
-        serviceCollection.AddSingleton(new BroadcasterId(twitchSecrets.BroadcasterId));
+        serviceCollection.AddSingleton(_twitchApi);
+        serviceCollection.AddSingleton(new BroadcasterId(_twitchSecrets.BroadcasterId));
         serviceCollection.AddSingleton(discordSecrets);
         
         SlashCommandsExtension slashCommands = discordClient.UseSlashCommands(new SlashCommandsConfiguration()
@@ -86,5 +100,30 @@ internal class Program
         
         await discordClient.ConnectAsync();
         await Task.Delay(-1);
+    }
+
+    public static async Task RefreshTwitchToken()
+    {
+        ValidateAccessTokenResponse response = await _twitchApi.Auth.ValidateAccessTokenAsync();
+
+        if (response is not null && response.ExpiresIn > 180)
+            return;
+
+        Console.WriteLine("Refreshing the no longer valid / about to expire Twitch auth token");
+
+        try
+        {
+            RefreshResponse refreshResponse = await _twitchApi.Auth.RefreshAuthTokenAsync(_twitchRefreshToken, _twitchSecrets.Secret, _twitchSecrets.ClientId);
+            _twitchRefreshToken = refreshResponse.RefreshToken;
+            _twitchApi.Settings.AccessToken = refreshResponse.AccessToken;
+            _twitchSecrets.RefreshToken = refreshResponse.RefreshToken;
+            _twitchSecrets.AccessToken = refreshResponse.AccessToken;
+            _twitchSecrets.Save();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("WARNING: Twitch token could not be refreshed, the application will probably stop working");
+            Console.WriteLine(e);
+        }
     }
 }
--- SAESRPredictionsBot/TwitchSecrets.cs
+++ SAESRPredictionsBot/TwitchSecrets.cs
@@ -1,11 +1,38 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
 namespace SAESRPredictionsBot;
 
 public class TwitchSecrets
 {
+    private const string FileName = "/config/twitch-secrets";
+    
     public string ClientId { get; set; }
     public string AccessToken { get; set; }
+    public string RefreshToken { get; set; }
     public string Secret { get; set; }
     public string BroadcasterId { get; set; }
+    
+    public static TwitchSecrets Load()
+    {
+        string json = File.ReadAllText(FileName);
+        return JsonConvert.DeserializeObject<TwitchSecrets>(json)!;
+    }
+    
+    public void Save()
+    {
+        string json = JsonConvert.SerializeObject(this, new JsonSerializerSettings
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            Formatting = Formatting.Indented
+        });
+        
+        File.WriteAllText(FileName, json);
+    }
+
+    private TwitchSecrets()
+    {
+    }
 }
 
 public class BroadcasterId
